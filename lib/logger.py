52ac760d320bb119876ff2488a82be0d65a9a124
Nataly Maksimova <sahaling70@gmail.com>
2022-01-18 14:38:14 +0500

    Ex 15, Ex 16, logger.py, my_requests.py

---
+++ lib/logger.py
@@ -0,0 +1,46 @@
+import requests
+import datetime
+import os
+
+
+class Logger:
+
+    file_name = f"Logs/log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))+ ".log"
+
+    @classmethod
+    def __write_log_to_file(cls, data: str):
+        with open(cls.file_name, 'a', encoding='utf-8') as logger_file:
+            logger_file.write(data)
+
+
+    @classmethod
+    def add_request(cls, url: str, data: dict, headers: dict, cookies: dict, method: str):
+        testname = os.environ.get('PYTEST_CURRENT_TEST')
+
+        data_to_add = "\n-----\n"
+        data_to_add += f"Test: {testname}\n"
+        data_to_add += f"Time: {str(datetime.datetime.now())}\n"
+        data_to_add += f"Request method: {method}\n"
+        data_to_add += f"Request URL: {url}\n"
+        data_to_add += f"Request data: {data}\n"
+        data_to_add += f"Request headers: {headers}\n"
+        data_to_add += f"Request cookies: {cookies}\n"
+        data_to_add += "\n"
+
+        cls.__write_log_to_file(data_to_add)
+
+
+    @classmethod
+    def add_response(cls, response: requests.Response):
+        cookies_as_dict = dict(response.cookies)
+        headers_as_dict = dict(response.headers)
+
+        data_to_add = f"Response code: {response.status_code}\n"
+        data_to_add += f"Response text: {response.text}\n"
+        data_to_add += f"Response headers: {headers_as_dict}\n"
+        data_to_add += f"Response cookies: {cookies_as_dict}\n"
+        data_to_add += "\n-----\n"
+
+        cls.__write_log_to_file(data_to_add)
+
+
